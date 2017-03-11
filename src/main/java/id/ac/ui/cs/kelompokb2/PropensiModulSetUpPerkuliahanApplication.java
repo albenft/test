2312c4fe8162388aae29a654266850d762dd38d5
Alben <Alben@alben>
2017-03-12 03:10:12 +0700

    First Set Up

---
+++ src/main/java/id/ac/ui/cs/kelompokb2/PropensiModulSetUpPerkuliahanApplication.java
@@ -0,0 +1,12 @@
+package id.ac.ui.cs.kelompokb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PropensiModulSetUpPerkuliahanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PropensiModulSetUpPerkuliahanApplication.class, args);
+	}
+}
